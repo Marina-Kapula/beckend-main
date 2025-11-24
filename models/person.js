54f220b3885d1f3bbed2861115fa03699bd947f0
Maryna Kapula <maryna.kapula@gmail.com>
2025-11-24 12:31:45 +0200

    3.19 puhelinluettelo ja tietokanta, step7

--- models/person.js
+++ models/person.js
@@ -1,7 +1,25 @@
-// models/person.js
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-module.exports = mongoose.model('Person', personSchema);
+  name: {
+    type: String,
+    minlength: 3,            // имя минимум 3 символа
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,            // номер минимум 8 символов
+    required: true,
+    validate: {
+      validator: function (v) {
+        // 2–3 цифры, дефис, потом ещё цифры (например 09-1234556, 040-22334455)
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message:
+        props =>
+          `${props.value} is not a valid phone number! Use form XX-XXXXXXX or XXX-XXXXXXX.`,
+    },
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)
